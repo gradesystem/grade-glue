9c4705fc713f4f7e0d21de1307e54b60802c82b3
eblondel <emmanuel.blondel1@gmail.com>
2014-12-22 17:46:57 +0100

    #4 deprecate ows stuff + preparation for EEZ mappings using CsvAsset

--- src/main/java/org/sticky/EezGlue.java
+++ src/main/java/org/sticky/EezGlue.java
@@ -1,29 +1,58 @@
 package org.sticky;
 
-import java.io.InputStream;
+import static org.sticky.Glues.load;
 
 import org.junit.Test;
+import org.virtualrepository.csv.CsvAsset;
+import org.virtualrepository.csv.CsvTable;
+import org.virtualrepository.ows.Features;
 import org.virtualrepository.ows.WfsFeatureType;
+import org.virtualrepository.tabular.Row;
+import org.virtualrepository.tabular.Table;
 
 public class EezGlue {
 
+	/**
+	 * Requires investigation about Apache-Sis bug
+	 * Switched off for now
+	 * 
+	 */
+	@Deprecated
 	@Test
-	public void grabMarineRegions() {
+	public void grabEez() {
 		
 		WfsFeatureType asset = new WfsFeatureType("marine-regions","MarineRegions:eez");
 		
 		asset.setService(Glues.vliz);
 		
-		InputStream stream = Glues.repository.retrieve(asset, InputStream.class);
+		//retrieve features
+		Features features = Glues.repository.retrieve(asset, Features.class);
 		
-		Glues.store("marine-regions.xml",stream);
+		//write features to gml
+		Glues.storeAsGml("marine-regions.xml",features);		
+
 	}
 	
-
 	@Test
-	public void pushMarineRegions() {
-	
+	public void readEez(){
 		
+		CsvAsset asset = new CsvAsset("someid","marine-regions");
+		
+		asset.hasHeader(true);
+		asset.setDelimiter(',');
+
+		Table table = new CsvTable(asset, load("marine-regions.txt"));
+		
+		for (Row row : table)
+			System.out.println(row);
+		
+	}
+	
+	
+	@Deprecated
+	@Test
+	public void pushEez() {
+	
 		WfsFeatureType asset = new WfsFeatureType("marine-regions","marine-regions",Glues.grade);
 		
 		Glues.repository.publish(asset,Glues.load("marine-regions.xml"));
