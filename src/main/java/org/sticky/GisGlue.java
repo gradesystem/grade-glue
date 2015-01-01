f0971cbbfb20d5a0c14af1a4617c04b1fd8225c6
eblondel <emmanuel.blondel1@gmail.com>
2015-01-01 16:07:38 +0100

    #6 feeding Grade with georefs - species / species distributions

---
+++ src/main/java/org/sticky/GisGlue.java
@@ -0,0 +1,166 @@
+package org.sticky;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
+import static javax.ws.rs.client.ClientBuilder.newClient;
+import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
+import static org.sticky.Glues.*;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Predicate;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientResponseContext;
+import javax.ws.rs.client.ClientResponseFilter;
+import javax.ws.rs.client.WebTarget;
+
+import org.fao.fi.comet.mapping.model.Mapping;
+import org.fao.fi.comet.mapping.model.MappingData;
+import org.fao.fi.comet.mapping.model.MappingElement;
+
+import static org.fao.fi.comet.mapping.dsl.MappingDSL.map;
+import static org.fao.fi.comet.mapping.dsl.MappingDetailDSL.target;
+import static org.fao.fi.comet.mapping.dsl.MappingElementDSL.wrap;
+import static org.fao.fi.comet.mapping.model.utils.jaxb.JAXB2DOMUtils.asElement;
+
+import org.geotoolkit.wms.xml.AbstractLayer;
+import org.geotoolkit.wms.xml.v130.Identifier;
+import org.geotoolkit.wms.xml.v130.Layer;
+import org.geotoolkit.wms.xml.v130.MetadataURL;
+import org.geotoolkit.wms.xml.v130.WMSCapabilities;
+import org.glassfish.jersey.client.filter.EncodingFilter;
+import org.glassfish.jersey.message.GZipEncoder;
+import org.junit.Test;
+import org.sticky.jaxb.Code;
+import org.sticky.jaxb.Entity;
+import org.sticky.jaxb.GeographicReference;
+import org.virtualrepository.comet.CometAsset;
+
+/**
+ * Glue to push Gis mappings to Grade. By Gis mapping, we mean the mapping
+ * between LOD domain object URIs (that have been used by GEMS to annotate OGC
+ * services, data & metadata assets), and GIS product URIs (represented by the
+ * xml OGC metadata published in Geonetwork)
+ * 
+ * 
+ * @author Emmanuel Blondel
+ *
+ */
+public class GisGlue {
+	
+	@Test
+	public void grabSpeciesDistributions(){
+		
+		String ep = "http://www.fao.org/figis/geoserver/species/ows";
+		
+		MappingData out = buildGeographicReferences(ep);
+		
+		store("species-distributions.xml", out);
+		
+	}
+	
+	@Test
+	public void pushSpeciesDistributions(){
+		
+		CometAsset asset = new CometAsset("species-distributions", grade);
+		
+		repository.publish(asset, load("species-distributions.xml"));
+		
+	}
+	
+	/**
+	 * Builds the mapping between a FLOD domain object uri and a Geonetwork
+	 * GIS metadata uri
+	 * 
+	 * @return the mapping in comet format
+	 */
+	static MappingData buildGeographicReferences(String endpoint){
+		
+		//read WMS GetCapabilities document
+		Client client = newClient()
+				.register(ContentTypeFixer.instance)
+				.register(GZipEncoder.class)
+				.register(EncodingFilter.class);
+		
+		WebTarget target = client.target(endpoint)
+								.queryParam("service", "wms")
+								.queryParam("version", "1.3.0")
+								.queryParam("request", "GetCapabilities");
+		
+		WMSCapabilities capabilities = target.request().get(WMSCapabilities.class);
+		
+		//prepare mapping data
+		MappingData mappingData = new MappingData();
+		mappingData.setDescription("Mapping between LOD entities and geographic references from "
+								  +"the following GIS collection: "+capabilities.getService().getTitle());
+		mappingData.setVersion("1.0");
+		mappingData.setProducedOn(new Date());
+		mappingData.setProducedBy("Emmanuel Blondel");
+		
+		//extract relevant piece of information and build the comet mapping on-the-fly
+		Iterator<AbstractLayer> it = capabilities.getLayers().iterator();
+		while(it.hasNext()){
+
+			Layer layer = (Layer) it.next();
+			
+			//LOD reference
+			Identifier id = null;
+			if(layer.getIdentifier().size() > 0){
+				
+				List<Identifier> ids = layer.getIdentifier().stream()
+										  .filter(p -> p.getAuthority().equals("FLOD"))
+										  .collect(toList());
+				if(ids.size() > 0)
+					id = ids.get(0);
+			}
+			
+			//GIS reference
+			MetadataURL md = null;
+			if(layer.getMetadataURL().size() > 0){
+				
+				Predicate<MetadataURL> formatPred = p -> p.getFormat().equals("text/xml");
+				Predicate<MetadataURL> typePred = p -> p.getType().equals("ISO19115:2003");
+				List<MetadataURL> mds = layer.getMetadataURL().stream()
+										   .filter(formatPred.and(typePred))
+										   .collect(toList());
+				if(mds.size() > 0)
+					md = mds.get(0);
+			}
+			
+			// comet mapping
+			if (id != null && md != null) {
+				MappingElement source = wrap(asElement(Entity.coding(
+						new Code(id.getValue(), "uri", null))));
+				Mapping mappingEntry = map(source);
+
+				mappingEntry.to(target(wrap(asElement(GeographicReference.coding(
+						new Code(md.getOnlineResource().getHref(),"uri", layer.getTitle()))))));
+				mappingData.include(mappingEntry);
+			}
+
+		}	
+		
+		return mappingData;
+	}
+	
+
+	private static class ContentTypeFixer implements ClientResponseFilter  {
+		
+		static final ContentTypeFixer instance = new ContentTypeFixer();
+		
+		@Override
+		public void filter(ClientRequestContext requestContext,
+				ClientResponseContext responseContext) throws IOException {
+			
+			if (responseContext.getHeaderString(CONTENT_TYPE).contains("subtype=gml"))
+				responseContext.getHeaders().put(CONTENT_TYPE, asList("text/xml"));
+			
+		}
+	}
+	
+
+}
---
+++ src/main/java/org/sticky/jaxb/GeographicReference.java
@@ -0,0 +1,99 @@
+package org.sticky.jaxb;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * A simple GeographicReference JAXB bean to use for producing comet code mappings dealing
+ * with a GeographicReference and encapsulate a uri identifying this geographic reference
+ * 
+ * @author Emmanuel Blondel
+ *
+ */
+@XmlRootElement(name = "GeographicReference")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class GeographicReference implements Serializable {
+	
+	private static final long serialVersionUID = 4309404806843871599L;
+	
+	@XmlElement(name = "Code")
+	private Code _code;
+
+	/**
+	 * Class constructor
+	 *
+	 */
+	public GeographicReference() {
+	}
+
+	/**
+	 * Class constructor
+	 *
+	 * @param code
+	 */
+	public GeographicReference(Code code) {
+		super();
+		this._code = code;
+	}
+
+	final static public GeographicReference coding(Code code) {
+		return new GeographicReference(code);
+	}
+
+	/**
+	 * @return the 'code' value
+	 */
+	public Code getCode() {
+		return this._code;
+	}
+
+	/**
+	 * @param code
+	 *            the 'code' value to set
+	 */
+	public void setCode(Code code) {
+		this._code = code;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime
+				* result
+				+ ((this._code == null) ? 0 : this._code
+						.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GeographicReference other = (GeographicReference) obj;
+		if (this._code == null) {
+			if (other._code != null)
+				return false;
+		} else if (!this._code.equals(other._code))
+			return false;
+		return true;
+	}
+}
